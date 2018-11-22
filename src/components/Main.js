da7c8860a6b4e041a7c4521e5706bde616e026fb
jeff <jeffreymj94@gmail.com>
2018-11-22 11:35:39 -0800

    change search method from specific player id to name

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {ShotChart} from "./ShotChart";
 import {Profile} from "./Profile";
+import nba from 'nba';
 
 export class Main extends React.Component {
     state = {
-        playerId: 201939,
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+
     }
+
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((e) => console.log(e))
+    }
+
     render() {
         return (
             <div className="main">

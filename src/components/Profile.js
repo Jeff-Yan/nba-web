2b96ecde9ba758111f9492bd037c6856522a39d9
jeff <jeffreymj94@gmail.com>
2018-11-22 11:56:32 -0800

    add team logo

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {PROFILE_PIC_URL_PREFIX} from "../constants";
+import {PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX} from "../constants";
 
 export class Profile extends React.Component {
     render() {
-        const {playerId, displayerFirstLast} = this.props.playerInfo;
+        const {playerId, displayerFirstLast, teamAbbreviation} = this.props.playerInfo;
         return (
           <div className="profile">
               <img
                   className="player-image"
                   src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
-                  alt={displayerFirstLast}/>
+                  alt={displayerFirstLast}
+              />
+              <img
+                  className="team-logo"
+                  src={`${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`}
+                  alt={displayerFirstLast}
+              />
           </div>
         );
     }

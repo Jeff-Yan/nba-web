02e958c53273c5cf95d4f6d4e0d191a69976cbb8
jeff <jeffreymj94@gmail.com>
2018-11-22 11:12:13 -0800

    Add the profile test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {ShotChart} from "./ShotChart";
+import {Profile} from "./Profile";
 
-export class Main extends React.Component {render() {
+export class Main extends React.Component {
+    render() {
         return (
             <div className="main">
+                <Profile/>
                 <ShotChart playerId={2544}/>
             </div>
         );

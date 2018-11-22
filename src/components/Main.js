35f7bd26c5abce0c7c296877cdc1ca195d45909b
jeff <jeffreymj94@gmail.com>
2018-11-22 11:28:00 -0800

    add season of player

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,14 @@ import {ShotChart} from "./ShotChart";
 import {Profile} from "./Profile";
 
 export class Main extends React.Component {
+    state = {
+        playerId: 201939,
+    }
     render() {
         return (
             <div className="main">
-                <Profile/>
-                <ShotChart playerId={2544}/>
+                <Profile playerId={this.state.playerId}/>
+                <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }

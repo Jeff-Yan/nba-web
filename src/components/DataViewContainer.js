4bc1404e1d47bc2fea0798023036ea48025c0f79
Jeffrey <jeffreymj94@gmail.com>
2018-12-02 12:11:29 -0800

    debounce

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import _ from 'lodash';
+import { Row, Col, Radio } from 'antd';
 import { ShotChart } from './ShotChart';
 import { CountSlider } from './CountSlider';
+
+const RadioGroup = Radio.Group;
 
 export class DataViewContainer extends React.Component {
     state = {
         minCount: 2,
+        chartType: 'hexbin',
     }
 
     onMinCountChange = (minCount) => {
         this.setState({ minCount });
     }
+
+    onChartTypeChange = (e) => {
+        this.setState({ chartType: e.target.value });
+    }
+
     render() {
         return (
             <div className = "data-view">
                 <ShotChart
                     playerId={this.props.playerId}
                     minCount={this.state.minCount}
+                    chartType={this.state.chartType}
                 />
                 <div className="filters">
                     <Row className="filter-row">
                         <Col span={2} offset={3} className="filter-label" >Shots:</Col>
                         <Col span={16} className="filter-control">
-                            <CountSlider onMinCountChange={this.onMinCountChange}/>
+                            <CountSlider onMinCountChange={_.debounce(this.onMinCountChange, 500)} className="filter-control" />
                         </Col>
                     </Row>
+
                 </div>
 
 

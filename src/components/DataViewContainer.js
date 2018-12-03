062f4fcdf61a72203f06fe139e4784a368873ecc
Jeffrey <jeffreymj94@gmail.com>
2018-12-02 16:53:42 -0800

    add tooltip and clean code

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import { Row, Col, Radio } from 'antd';
+import { Radio, Switch, Row, Col } from 'antd';
 import { ShotChart } from './ShotChart';
 import { CountSlider } from './CountSlider';
 
@@ -10,6 +10,7 @@ export class DataViewContainer extends React.Component {
     state = {
         minCount: 2,
         chartType: 'hexbin',
+        displayTooltip: true,
     }
 
     onMinCountChange = (minCount) => {
@@ -20,33 +21,52 @@ export class DataViewContainer extends React.Component {
         this.setState({ chartType: e.target.value });
     }
 
+    onTooltipChange = (displayTooltip) => {
+        this.setState({ displayTooltip });
+    }
+
     render() {
         return (
-            <div className = "data-view">
+            <div className="data-view">
                 <ShotChart
                     playerId={this.props.playerId}
                     minCount={this.state.minCount}
                     chartType={this.state.chartType}
+                    displayTooltip={this.state.displayTooltip}
                 />
                 <div className="filters">
-                    <Row className="filter-row">
-                        <Col span={2} offset={3} className="filter-label" >Shots:</Col>
-                        <Col span={16} className="filter-control">
-                            <CountSlider onMinCountChange={_.debounce(this.onMinCountChange, 500)} className="filter-control" />
-                        </Col>
-                    </Row>
+                    {this.state.chartType === 'hexbin' ? (
+                        <Row className="filter-row">
+                            <Col span={2} offset={3} className="filter-label">Shots:</Col>
+                            <Col span={16}>
+                                <CountSlider
+                                    onMinCountChange={_.debounce(this.onMinCountChange, 500)} className="filter-control"
+                                />
+                            </Col>
+                        </Row>
+                    ) : null}
                     <Row className="filter-row">
                         <Col span={10} offset={3}>
-                            <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType} className="filter-control" >
+                            <RadioGroup
+                                onChange={this.onChartTypeChange}
+                                value={this.state.chartType}
+                                className="filter-control"
+                            >
                                 <Radio value="hexbin">Hexbin</Radio>
                                 <Radio value="scatter">Scatter</Radio>
                             </RadioGroup>
                         </Col>
+                        <Col span={2}>Tooltip:</Col>
+                        <Col span={3} className="filter-control">
+                            <Switch
+                                checkedChildren="On"
+                                unCheckedChildren="Off"
+                                defaultChecked
+                                onChange={this.onTooltipChange}
+                            />
+                        </Col>
                     </Row>
-
                 </div>
-
-
             </div>
         );
     }
--- src/components/ShotChart.js
+++ src/components/ShotChart.js
@@ -12,6 +12,7 @@ export class ShotChart extends React.Component {
         playerId: PropTypes.number.isRequired,
         minCount: PropTypes.number.isRequired,
         chartType: PropTypes.string.isRequired,
+        displayTooltip: PropTypes.bool.isRequired,
     }
 
     componentDidUpdate() {
@@ -19,6 +20,7 @@ export class ShotChart extends React.Component {
             PlayerID: this.props.playerId,
             Season: '2016-17',
         }).then((response) => {
+            console.log(response);
             const final_shots = response.shot_Chart_Detail.map(shot => ({
                 x: (shot.locX + 250) / 10,
                 y: (shot.locY + 50) / 10,
@@ -30,7 +32,10 @@ export class ShotChart extends React.Component {
             const courtSelection = d3.select("#shot-chart");
             courtSelection.html('');
             const chart_court = court().width(500);
-            const chart_shots = shots().shotRenderThreshold(this.props.minCount).displayToolTips(true).displayType(this.props.chartType);
+            const chart_shots = shots()
+                .shotRenderThreshold(this.props.minCount)
+                .displayToolTips(this.props.displayTooltip)
+                .displayType(this.props.chartType);
             courtSelection.call(chart_court);
             courtSelection.datum(final_shots).call(chart_shots);
         });
